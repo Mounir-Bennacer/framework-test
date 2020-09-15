54f6504f81c5bb45f08755c2b79e509db6518963
Mounir Bennacer <benphaoris@gmail.com>
2020-09-15 23:05:39 +0100

    admin can connect, adding routes/resources

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Companies;
 
 class EmployeesController extends Controller
 {
@@ -13,7 +14,9 @@ class EmployeesController extends Controller
      */
     public function index()
     {
-        //
+        $companies = Companies::all();
+        /* return view('companies.index', compact('companies')); */
+        return 'hello';
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('employees', 'EmployeesController');
+Route::resource('companies', 'CompaniesController');
